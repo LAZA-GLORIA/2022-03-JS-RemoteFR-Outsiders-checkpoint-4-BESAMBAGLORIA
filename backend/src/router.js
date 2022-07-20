6ebb641f2671b4e22880f374aa6df947a663b305
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-07-20 11:51:09 +0200

    commit before PR dans dev

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,19 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { UsersController } = require("./controllers");
 
-/**
- * router.get("/items", ItemController.browse);
-router.get("/items/:id", ItemController.read);
-router.put("/items/:id", ItemController.edit);
-router.post("/items", ItemController.add);
-router.delete("/items/:id", ItemController.delete);
- */
-
 router.post("/auth/users", UsersController.add);
-router.get("/users", /* authorization, isAdmin, */ UsersController.browse);
+router.get("/users", UsersController.browse);
 router.get("/users/mail", UsersController.sendMail);
 router.post("/login/users", UsersController.login);
-router.get("/logout/users", /* authorization, */ UsersController.logout);
+router.get("/logout/users", UsersController.logout);
 router.get("/users/:id", UsersController.read);
 router.delete("/users/:id", UsersController.delete);
 router.put("/infos/users/:id", UsersController.editInfos);
